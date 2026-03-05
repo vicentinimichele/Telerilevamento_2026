e982f1b6e25df480afdcdfe7e8fb1dab18354995
Michele Vicentini <78214095+vicentinimichele@users.noreply.github.com>
2026-03-05 15:20:31 +0100

    Enhance image visualization with additional bands
    Added imports and plots for additional Sentinel-2 bands including green, red, and near infrared. Updated color palettes for better visualization.

--- Code/02_visualizzazione_immagini.R
+++ Code/02_visualizzazione_immagini.R
@@ -12,7 +12,7 @@ im.list()
 
 # Bande sentinel https://gisgeography.com/sentinel-2-bands-combinations/ 
 
-# importare le bande che ci interessano
+# importare le bande che ci interessano colore blu
 b2 <- im.import("sentinel.dolomites.b2.tif")
 
 # Modificare la palet colori
@@ -43,3 +43,48 @@ dev.off()
 im.multiframe(1,2)
 plot(b2, col=inferno (100))
 plot(b2, col=cl)
+
+# Importare banda del verde b3
+b3 <- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col=magma (100))
+
+# Importare banda del rosso b4
+b4 <- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=magma (100))
+
+# Importare banda del near infrared b8
+b8 <- im.import("sentinel.dolomites.b8.tif")
+plot(b8, col=viridis (100))
+
+# Esercizio multiframe 4 bande, legende con i colori giusti
+b2 <- im.import("sentinel.dolomites.b2.tif")
+b3 <- im.import("sentinel.dolomites.b3.tif")
+b4 <- im.import("sentinel.dolomites.b4.tif")
+b8 <- im.import("sentinel.dolomites.b8.tif")
+cl2 <- colorRampPalette(c("dark blue", "blue", "light green"))(100)
+cl3 <- colorRampPalette(c("dark green", "green", "light green"))(100)
+cl4 <- colorRampPalette(c("red4", "red", "pink"))(100)
+cl8 <- colorRampPalette(c("palevioletred4", "sienna", "springgreen2"))(100)
+
+im.multiframe(2,2)
+plot(b2, col=cl2 )
+plot(b3, col=cl3 )
+plot(b4, col=cl4 )
+plot(b8, col=cl8 )
+
+im.multiframe(2,2)
+plot(b2, col=inferno (100))
+plot(b3, col=inferno (100))
+plot(b4, col=inferno (100))
+plot(b8, col=inferno (100))
+
+# stack per mettere tutte le immagini
+sentinel <- c(b2, b3, b4, b8)
+plot(sentinel, col=inferno (100))
+
+#per vedere solo una banda
+plot(sentinel$sentinel.dolomites.b8)
+
+#layer1=b2 layer2=b3, layer3=b4, layer4=b8
+plot(sentinel[[2]]) #layer 2 quindi la b3
+plot(sentinel[[4]]) #layer 2 quindi la b8
