2da2e81d0d371dc16227e041c8d3476eb207b0d5
Michele Vicentini <78214095+vicentinimichele@users.noreply.github.com>
2026-03-10 17:45:50 +0100

    Update image visualization script with new libraries
    Added libraries for visualization and enhanced image plotting functions.

--- Code/02_visualizzazione_immagini.R
+++ Code/02_visualizzazione_immagini.R
@@ -5,7 +5,9 @@ install.packages("viridis")
 
 library(terra) # per usare i dati spaziali
 library(imageRy) # pacchetto per le immagini satellitari
-library(viridis)
+library(viridis) # colori per cose
+library(ggplot2) # grafici
+library(patchwork)
 
 # Ogni funzione di imageRy inizia con im.
 im.list()
@@ -88,3 +90,72 @@ plot(sentinel$sentinel.dolomites.b8)
 #layer1=b2 layer2=b3, layer3=b4, layer4=b8
 plot(sentinel[[2]]) #layer 2 quindi la b3
 plot(sentinel[[4]]) #layer 2 quindi la b8
+
+b2 <- im.import("sentinel.dolomites.b2.tif")
+b3 <- im.import("sentinel.dolomites.b3.tif")
+b4 <- im.import("sentinel.dolomites.b4.tif")
+b8 <- im.import("sentinel.dolomites.b8.tif")
+
+im.ggplot(b8)
+
+# per plottare più immagini, li posso unire trattandoli come oggetti
+
+p1 <- im.ggplot(b8)
+p2 <- im.ggplot(b4)
+
+p1 + p2
+
+# Metodi Multiframe
+# 1. par(mfrow=c(1,2))
+# 2. im.multiframe(1,2)
+# 3. stack
+# 4. ggplot2 patchwork
+
+# RGB plotting
+sentinel <- c(b2, b3, b4, b8)
+
+# 1=b2 blu
+# 2=b3 verde
+# 3=b4 rosso
+# 4=b5 infrarosso vicino
+
+# tre filtri ma con 4 bande
+
+# creiamo una per il visibile
+im.plotRGB(sentinel, r=3, g=2, b=1) # funzione per plottare a colori naturali
+
+# spostiamo le bande e i colori per RGB
+im.plotRGB(sentinel, r=4, g=3, b=2) # false colors, banda infrarosso apporta molte informazioni
+
+im.multiframe(1,2)
+im.plotRGB(sentinel, r=3, g=2, b=1) # funzione per plottare a colori naturali
+im.plotRGB(sentinel, r=4, g=3, b=2) # false colors
+
+plot(sentinel[[4]])
+im.plotRGB(sentinel, r=4, g=3, b=2) # false colors
+
+im.plotRGB(sentinel, r=3, g=4, b=2) # nir (infrarosso) nel verde  
+im.plotRGB(sentinel, r=3, g=2, b=4) # nir nel blu, utile per vedere la deforestazione
+
+# multi frame con le 4 visualizzazioni possibili
+im.multiframe(2,2)
+im.plotRGB(sentinel, r=3, g=2, b=1)
+im.plotRGB(sentinel, r=4, g=3, b=2)
+im.plotRGB(sentinel, r=3, g=4, b=2) # nir (infrarosso) nel verde  
+im.plotRGB(sentinel, r=3, g=2, b=4)
+
+# se mantengo la banda nel nir nello stesso posto ma cambio gli altri colori non cambia praticamente nulla
+
+# per fare un grafico a correlazione?
+pairs(sentinel)
+
+# versione semplice della funzione
+im.plotRGB(sentinel, 4, 2, 3)
+
+# plotRGB da terra
+plotRGB(sentinel, 4, 2, 3, stretch = "lin")
+plotRGB(sentinel, 4, 2, 3, stretch = "hist")
+
+
+
+
